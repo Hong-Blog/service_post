38781f2ae5c3f7ffb1dc35cea6ea5cf26035c2ae
hong <lun3322@126.com>
2020-12-16 23:51:39 +0800

    新增数据库依赖

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("ok")
+	engine := gin.Default()
+
+	engine.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	engine.GET("/post", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test sql",
+		})
+	})
+
+	_ = engine.Run(":80")
 }
